67b6e9607edd8596e2d7acccda30545525c55d9f
log8450 <log8450@gmail.com>
2021-10-03 16:43:23 +0900

    25 create

---
+++ app/src/main/java/com/example/meethere/SelectLocation_2_5.kt
@@ -0,0 +1,11 @@
+package com.example.meethere
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SelectLocation_2_5 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_location25)
+    }
+}
