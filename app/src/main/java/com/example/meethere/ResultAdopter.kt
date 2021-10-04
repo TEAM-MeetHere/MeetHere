53188655fb2a647abb66c337461029a99ea618d7
log8450 <log8450@gmail.com>
2021-10-04 20:17:51 +0900

    2_8 Detail 화면 완성, 객체별 버튼으로 상세보기 이동 기능 구현 완료

--- app/src/main/java/com/example/meethere/ResultAdopter.kt
+++ app/src/main/java/com/example/meethere/ResultAdopter.kt
@@ -4,21 +4,44 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.meethere.databinding.ItemResultBinding
 import kotlinx.android.synthetic.main.item_result.view.*
+import android.widget.Toast
+
+import android.R
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import androidx.core.content.ContextCompat.startActivity
+import java.security.AccessController.getContext
+
 
 class ResultAdopter (
     private val results: MutableList<Result>
 ) : RecyclerView.Adapter<ResultAdopter.ResultViewHolder>() {
-    class ResultViewHolder(itemView: View) : RecyclerView.ViewHolder (itemView)
+    class ResultViewHolder(val binding: ItemResultBinding) : RecyclerView.ViewHolder (binding.root) {
+        val btn = binding.btnDetail
+
+        init {
+            binding.btnDetail.setOnClickListener {
+                fun OnClick(view: View) {
+                    Toast.makeText(view.context, "Item is clicked", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ResultViewHolder {
-        return ResultViewHolder(
+        val binding = ItemResultBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        /*return ResultViewHolder(
             LayoutInflater.from(parent.context).inflate(
                 R.layout.item_result,
                 parent,
                 false
             )
-        )
+        )*/
+        return ResultViewHolder(binding)
     }
 
     /*   private fun toggleStrikeThrough(textViewAddress: TextView, textViewName: TextView) {
@@ -35,9 +58,23 @@ class ResultAdopter (
             textViewName.text = curAddress.Name
             textViewTime.text = curAddress.Time.toString()
         }
+
+        holder.btn.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(p0: View?) {
+/*                Log.d("sss","Test btn${results[position].Name}")
+                if(results.isNotEmpty())
+                    results.remove(results[position])
+                notifyDataSetChanged()*/
+                val context=holder.itemView.context
+                val intent = Intent(context, ShowDetail_2_8::class.java)
+                context.startActivity(intent)
+            }
+        })
     }
 
     override fun getItemCount(): Int {
         return results.size
     }
+
+
 }
--- app/src/main/java/com/example/meethere/ShowResult_2_7.kt
+++ app/src/main/java/com/example/meethere/ShowResult_2_7.kt
@@ -1,13 +1,23 @@
 package com.example.meethere
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.meethere.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_set_location24.*
 import kotlinx.android.synthetic.main.activity_show_result27.*
+import kotlinx.android.synthetic.main.item_result.*
 
 class ShowResult_2_7 : AppCompatActivity() {
     private lateinit var resultAdapter: ResultAdopter
+
+    private fun startToDetailActivity(id: Int, message: String) {
+        val intent = Intent(applicationContext, ShowDetail_2_8::class.java)
+        startActivity(intent)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,5 +34,16 @@ class ShowResult_2_7 : AppCompatActivity() {
         resultAdapter.addResult(resultObject2)
         val resultObject3 = Result("최현호",120)
         resultAdapter.addResult(resultObject3)
+
+        buttonSave.setOnClickListener {
+            Toast.makeText(this@ShowResult_2_7, "저장하기", Toast.LENGTH_SHORT).show()
+        }
+
+        buttonShare.setOnClickListener {
+            Toast.makeText(this@ShowResult_2_7, "공유하기", Toast.LENGTH_SHORT).show()
+        }
+
     }
+
+
 }
