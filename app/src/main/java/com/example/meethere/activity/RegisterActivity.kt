e170c94c25a9085fcfecc9498b8d28fdbc3d9327
log8450 <log8450@gmail.com>
2021-10-22 20:39:10 +0900

    회원가입시 주소 입력 칸 반영

--- app/src/main/java/com/example/meethere/activity/RegisterActivity.kt
+++ app/src/main/java/com/example/meethere/activity/RegisterActivity.kt
@@ -1,19 +1,39 @@
 package com.example.meethere.activity
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.example.meethere.R
 import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
+    var resultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                // There are no request codes
+                val data: Intent? = result.data
+                if (data != null) {
+                    if (data.hasExtra("data")) {
+                        register__address.setText(data.getStringExtra("data"))
+                    }
+                }
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
-        auth_ID.setOnClickListener({
+        auth_ID.setOnClickListener {
             var intent = Intent(this, AuthIDActivity::class.java)
             startActivity(intent)
-        })
+        }
+
+        register__address.setOnClickListener {
+            val intent = Intent(this, WebViewActivity::class.java)
+            resultLauncher.launch(intent)
+        }
     }
 }
