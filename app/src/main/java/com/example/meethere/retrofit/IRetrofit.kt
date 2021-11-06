028bcc2572de1eba03a08fd5453f57b2857b4526
choi1087 <adcdm87@gmail.com>
2021-11-07 00:14:14 +0900

    Retrofit update

--- app/src/main/java/com/example/meethere/retrofit/IRetrofit.kt
+++ app/src/main/java/com/example/meethere/retrofit/IRetrofit.kt
@@ -4,21 +4,13 @@ import com.example.meethere.retrofit.request.*
 import com.example.meethere.utils.API
 import com.google.gson.JsonElement
 import retrofit2.Call
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.POST
-import retrofit2.http.Query
+import retrofit2.http.*
 
 interface IRetrofit {
 
     //로그인
     @POST(API.LOGIN)
     fun loginService(@Body login: Login)
-            : Call<JsonElement>
-
-    //즐겨찾기 목록
-    @GET(API.BOOKMARK_LIST)
-    fun bookmarkListService(@Query("memberId") memberId: Long)
             : Call<JsonElement>
 
     //회원가입
@@ -50,23 +42,69 @@ interface IRetrofit {
     )
             : Call<JsonElement>
 
+    //회원 탈퇴
+    @DELETE(API.DELETE_MEM)
+    fun deleteMemberService(@Query("memberId") memberId: Long)
+            : Call<JsonElement>
+
+    //즐겨찾기 저장
     @POST(API.SAVE_BOOKMARK)
     fun saveBookmarkService(@Body bookmark: Bookmark)
             : Call<JsonElement>
 
+    //즐겨찾기 목록
+    @GET(API.BOOKMARK_LIST)
+    fun bookmarkListService(@Query("memberId") memberId: Long)
+            : Call<JsonElement>
+
+    //해당 즐겨찾기 출발 리스트
+    @GET(API.FIND_START_ADDRESS_LIST)
+    fun findStartAddressListService(@Query("bookmarkId") bookmarkId: Long)
+            : Call<JsonElement>
+
+    //즐겨찾기 삭제
+    @DELETE(API.DELETE_BOOKMARK)
+    fun deleteBookmarkService(@Query("bookmarkId") bookmarkId: Long)
+            : Call<JsonElement>
+
+    //공유코드 저장
     @POST(API.SAVE_SHARE)
     fun saveShareService(@Body share: Share)
             : Call<JsonElement>
 
+    //공유코드 도착주소 불러오기
     @GET(API.SHARE_DESTINATION)
     fun shareDestinationService(@Query("code") code: String)
             : Call<JsonElement>
 
+    //공유코드 출발주소 가져오기
     @GET(API.SHARE_START)
     fun shareStartService(@Query("shareId") shareId: Long)
             : Call<JsonElement>
 
-    @GET(API.FIND_START_ADDRESS_LIST)
-    fun findStartAddressListService(@Query("bookmarkId") bookmarkId: Long)
+    //친구 찾기
+    @GET(API.FIND_FRIEND)
+    fun findFriendService(
+        @Query("email") email: String,
+        @Query("name") name: String,
+        @Query("phone") phone: String
+    ): Call<JsonElement>
+
+    //친구 추가
+    @POST(API.ADD_FRIEND)
+    fun addFriendService(
+        @Query("membrId") memberId: Long,
+        @Query("friendId") friendId: Long
+    ): Call<JsonElement>
+
+    //친구 목록
+    @GET(API.FRIEND_LIST)
+    fun friendListService(@Query("memberId") memberId: Long)
             : Call<JsonElement>
+
+    //친구 삭제
+    @DELETE(API.DELETE_FRIEND)
+    fun deleteFriendService(@Query("friendId") friendId: Long)
+            : Call<JsonElement>
+
 }
